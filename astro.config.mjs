9cc0b1b5d0dc458bca55607ae535d035a00211ea
Ruben <ruben.onsurbe@gmail.com>
2026-06-12 16:50:07 +0200

    chore: increase vercel function maxDuration to 60s

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  adapter: vercel(),
+  adapter: vercel({ maxDuration: 60 }),
   integrations: [tailwind(), icon()]
 });
