14e6fb2484438e2b81a1741d2228e5da178e70ce
Ruben <ruben.onsurbe@gmail.com>
2026-06-12 16:46:50 +0200

    chore: configure Vercel adapter, add .npmrc, and ignore .vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  adapter: node({ mode: 'middleware' }),
+  adapter: vercel(),
   integrations: [tailwind(), icon()]
 });
